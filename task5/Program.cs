22ad279cc487e8c1cf0e709d07a2740dcd2a8707
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-09 14:10:28 +0500

    task5

---
+++ task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program{
+    static void Main(){
+        System.Console.Write("x = ");
+        int x = int.Parse(Console.ReadLine());
+        System.Console.Write("y = ");
+        int y = int.Parse(Console.ReadLine());
+
+        Swap(ref x, ref y);
+        Console.WriteLine($"x = {x}");
+        Console.WriteLine($"y = {y}");
+    }
+
+    static void Swap(ref int a, ref int b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+}
