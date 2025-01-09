55fc1cd5ee5e76b0aa39614f6a3614150e2f2ac2
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-09 14:50:33 +0500

    task 10

---
+++ task10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main() {
+        Console.Write("Enter first number: ");
+        int val1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        int val2 = int.Parse(Console.ReadLine());
+
+        int result = Max(val1, val2);
+        Console.WriteLine(result);
+    }
+
+    static int Max(int val1, int val2) {
+        if (val1 < val2) {
+            return val2;
+        }
+        else {
+            return val1;
+        }
+    }
+}
