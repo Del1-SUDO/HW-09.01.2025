f34dde127adc2ec289a903fe02ad96db60a08d0c
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-09 14:48:28 +0500

    task 9

---
+++ task9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main() {
+        Console.Write("Enter first number: ");
+        int val1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        int val2 = int.Parse(Console.ReadLine());
+
+        int result = Min(val1, val2);
+        Console.WriteLine(result);
+    }
+
+    static int Min(int val1, int val2) {
+        if (val1 < val2) {
+            return val1;
+        }
+        else {
+            return val2;
+        }
+    }
+}
