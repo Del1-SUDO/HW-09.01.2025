ebb54f7239fd03787918c16ff995c6308d2450f3
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-09 14:25:35 +0500

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program{
+    static void Main(){
+        Console.Write("Enter your number: ");
+        int x = int.Parse(Console.ReadLine());
+
+        Divisors(x);
+    }
+
+    static void Divisors(int x){
+        for (int i=1; i<=x; i++){
+            if (x%i==0){
+                Console.Write(i + " ");
+            }
+        }
+    }
+
+}
