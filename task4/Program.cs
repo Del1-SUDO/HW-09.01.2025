246cfd0946a8752a77b4074538a7b8cfa2a1c876
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-09 13:58:57 +0500

    task 4

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program{
+    static void Main(){
+        Console.Write("x = ");
+        int x = int.Parse(Console.ReadLine());
+        Console.Write("y = ");
+        int y = int.Parse(Console.ReadLine());
+
+        int result = Pow(x, y);
+        Console.WriteLine($"{result}");
+    }
+
+    static int Pow(int x, int y){
+        return (int)Math.Pow(x, y);
+    }
+
+}
