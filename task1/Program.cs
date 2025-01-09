4c973bae9f89b3c922d0c91d9cf5c102f943811d
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-09 12:35:16 +0500

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+class Program
+{
+    static int Even(int n)
+    {
+        int count = 0;
+        foreach (char digit in n.ToString())
+        {
+            if ((digit - '0') % 2 == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static int Odd(int n)
+    {
+        int count = 0;
+        foreach (char digit in n.ToString())
+        {
+            if ((digit - '0') % 2 != 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static int Count(int n)
+    {
+        return n.ToString().Length;
+    }
+
+    static int Sum(int n)
+    {
+        int sum = 0;
+        foreach (char digit in n.ToString())
+        {
+            sum += digit - '0';
+        }
+        return sum;
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Even:{Even(n)}");
+        Console.WriteLine($"Odd:{Odd(n)}");
+        Console.WriteLine($"Digit:{Count(n)}");
+        Console.WriteLine($"Sum:{Sum(n)}");
+    }
+}
